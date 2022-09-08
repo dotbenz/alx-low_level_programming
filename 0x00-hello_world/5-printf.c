1427de4ee64d1b0d3aa00c8103effe1663e0695d
Ebenezer Adedotun <dotbenzer@yahoo.com>
2022-09-08 15:32:34 -0700

    made a little fix to my task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
-#include <std.io>
+#include <stdio.h>
 
 int main(void);
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
