022146a3ffc0fb414cb3c51261e2a73059c8806b
Ebenezer Adedotun <dotbenzer@yahoo.com>
2022-09-13 04:32:41 -0700

    my alx function task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints putchar
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char p[] = "_putchar";
+
+	unsigned int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(p[c]);
+	}
+
+		_putchar('\n');
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# This is a Readme file for 0x02-functions_nested_loops
