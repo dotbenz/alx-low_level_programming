312ccf9a7d7758e929bff2c2f7a817f9d308456d
Ebenezer Adedotun <dotbenzer@yahoo.com>
2022-09-13 06:21:44 -0700

    adeed task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
  * Return: void
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int i, j;
 
@@ -17,5 +17,6 @@ void print_alphabet_x10(void);
 			_putchar(j);
 		}
 		_putchar('\n');
+
 	}
 }
