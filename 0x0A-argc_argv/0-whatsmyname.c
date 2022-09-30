2c676833a75c6d8921aae08277fdbf53de7a5466
Ebenezer Adedotun <dotbenzer@yahoo.com>
2022-09-30 06:51:21 -0700

    solution to task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is a function that prints its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int main(int argc _attribute_((unused)), char *argv[])
+
+
+#endif /*MAIN_H*/
